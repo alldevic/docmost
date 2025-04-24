0b3e7b3b99063ad86306a9dd94f678ed646f3052
Nikolay Bely <belyynikolay@gmail.com>
2025-04-24 17:49:07 +0700

    1038 - column layout

--- apps/client/src/features/editor/extensions/extensions.ts
+++ apps/client/src/features/editor/extensions/extensions.ts
@@ -38,6 +38,8 @@ import {
   Embed,
   SearchAndReplace,
   Mention,
+  ColumnContainer,
+  Column,
 } from "@docmost/editor-ext";
 import {
   randomElement,
@@ -74,6 +76,8 @@ import i18n from "@/i18n.ts";
 import { MarkdownClipboard } from "@/features/editor/extensions/markdown-clipboard.ts";
 import EmojiCommand from "./emoji-command";
 import { CharacterCount } from "@tiptap/extension-character-count";
+import ColumnContainerView from "@/features/editor/components/column-layout/column-container-view";
+import ColumnView from "@/features/editor/components/column-layout/column-view";
 
 const lowlight = createLowlight(common);
 lowlight.register("mermaid", plaintext);
@@ -231,6 +235,12 @@ export const mainExtensions = [
       }
     },
   }).configure(),
+  ColumnContainer.configure({
+    view: ColumnContainerView,
+  }),
+  Column.configure({
+    view: ColumnView,
+  }),
 ] as any;
 
 type CollabExtensions = (provider: HocuspocusProvider, user: IUser) => any[];
