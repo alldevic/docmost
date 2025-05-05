53a8de35a4d90574987a8ebf400835aaa1a98a44
Nikolay Bely <belyynikolay@gmail.com>
2025-05-06 00:30:21 +0700

    373 - deactivate user

---
+++ apps/client/src/features/workspace/components/members/components/workspace-deactivated-members-table.tsx
@@ -0,0 +1,157 @@
+import {
+	Group,
+	Table,
+	Text,
+	Badge,
+	Button,
+	Popover,
+	Box,
+	Flex,
+	Tooltip,
+} from "@mantine/core";
+import {
+	useChangeMemberRoleMutation,
+	useDeactivateUserMutation,
+	useWorkspaceDeactivatedMembersQuery,
+	useWorkspaceMembersQuery,
+} from "@/features/workspace/queries/workspace-query.ts";
+import { CustomAvatar } from "@/components/ui/custom-avatar.tsx";
+import React, { FC } from "react";
+import RoleSelectMenu from "@/components/ui/role-select-menu.tsx";
+import {
+	getUserRoleLabel,
+	userRoleData,
+} from "@/features/workspace/types/user-role-data.ts";
+import useUserRole from "@/hooks/use-user-role.tsx";
+import { IRoleData, UserRole } from "@/lib/types.ts";
+import { IconTrash } from "@tabler/icons-react";
+import { ICurrentUser, IUser } from "../../../../user/types/user.types";
+import { useDisclosure } from "@mantine/hooks";
+import { useAtom } from "jotai";
+import { currentUserAtom } from "../../../../user/atoms/current-user-atom";
+import { useTranslation } from "react-i18next";
+
+interface WorkspaceDeactivatedMembersTableRowProps {
+	user: IUser;
+	isAdmin: boolean;
+	isOwner: boolean;
+	currentUser: ICurrentUser;
+}
+
+const WorkspaceDeactivatedMembersTableRow: FC<
+	WorkspaceDeactivatedMembersTableRowProps
+> = ({ user, isAdmin, isOwner, currentUser }) => {
+	const { t } = useTranslation();
+	const [opened, { open, close }] = useDisclosure(false);
+	const deactivateUserMutation = useDeactivateUserMutation();
+
+	const handleDelete = async (userId: string) => {
+		if (userId && currentUser?.workspace?.id)
+			await deactivateUserMutation.mutateAsync({
+				userId,
+				workspaceId: currentUser?.workspace?.id,
+			});
+	};
+
+	return (
+		<Table.Tr>
+			<Table.Td>
+				<Group gap="sm">
+					<CustomAvatar avatarUrl={user.avatarUrl} name={user.name} />
+					<div>
+						<Text fz="sm" fw={500}>
+							{user.name}
+						</Text>
+						<Text fz="xs" c="dimmed">
+							{user.email}
+						</Text>
+					</div>
+				</Group>
+			</Table.Td>
+
+			<Table.Td>
+				<Badge variant="light" color="gray">
+					Deactivated
+				</Badge>
+			</Table.Td>
+
+			{(isOwner || isAdmin) && (
+				<Table.Td>
+					<Popover
+						position="bottom"
+						withArrow
+						shadow="md"
+						opened={opened}
+						onClose={close}
+					>
+						<Popover.Target>
+							<Button
+								variant="subtle"
+								color="red"
+								onClick={open}
+								disabled={
+									// currentUser.user.role === "admin" && user.role === "owner"
+									true
+								}
+							>
+								<Tooltip label={t("Permanently delete member")}>
+									<IconTrash size={20} />
+								</Tooltip>
+							</Button>
+						</Popover.Target>
+						<Popover.Dropdown>
+							<Text>
+								{t("Are you sure? This action is <strong>not reversible</strong>")}
+							</Text>
+							<Flex justify={"space-evenly"} align={"center"} mt={8}>
+								<Button variant="outline" onClick={close}>
+									{t("Cancel")}
+								</Button>
+								<Button color="red" onClick={() => handleDelete(user?.id)}>
+									{t("Confirm")}
+								</Button>
+							</Flex>
+						</Popover.Dropdown>
+					</Popover>
+				</Table.Td>
+			)}
+		</Table.Tr>
+	);
+};
+
+export default function WorkspaceDeactivatedMembersTable() {
+	const { t } = useTranslation();
+	const { data, isLoading } = useWorkspaceDeactivatedMembersQuery({
+		limit: 100,
+	});
+	const { isAdmin, isOwner } = useUserRole();
+	const [currentUser] = useAtom(currentUserAtom);
+
+	return (
+		<>
+			{data && (
+				<Table verticalSpacing="sm">
+					<Table.Thead>
+						<Table.Tr>
+							<Table.Th>{t("User")}</Table.Th>
+							<Table.Th>{t("Status")}</Table.Th>
+							{(isOwner || isAdmin) && <Table.Th>{t("Action")}</Table.Th>}
+						</Table.Tr>
+					</Table.Thead>
+
+					<Table.Tbody>
+						{data?.items.map((user, index) => (
+							<WorkspaceDeactivatedMembersTableRow
+								key={index}
+								user={user}
+								isAdmin={isAdmin}
+								isOwner={isOwner}
+								currentUser={currentUser}
+							/>
+						))}
+					</Table.Tbody>
+				</Table>
+			)}
+		</>
+	);
+}
