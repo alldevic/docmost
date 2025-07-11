07acd48c84b98b430afb126195522dfd58275928
Nikolay Bely <belyynikolay@gmail.com>
2025-07-12 00:33:39 +0700

    679 - Every content block in table cell

--- apps/client/src/features/editor/extensions/extensions.ts
+++ apps/client/src/features/editor/extensions/extensions.ts
@@ -166,7 +166,9 @@ export const mainExtensions = [
     allowTableNodeSelection: true,
   }),
   TableRow,
-  TableCell,
+  TableCell.extend({
+    content: 'block+',
+  }),
   TableHeader,
   MathInline.configure({
     view: MathInlineView,
