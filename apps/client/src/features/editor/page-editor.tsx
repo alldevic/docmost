0facffc716eee36dc6d48118a26e2342efafe1d5
Nikolay Bely <belyynikolay@gmail.com>
2025-07-12 06:11:24 +0700

    1272 - Ctrl/Cmd+S: prevent 'Save As' dialog, force save

--- apps/client/src/features/editor/page-editor.tsx
+++ apps/client/src/features/editor/page-editor.tsx
@@ -211,6 +211,11 @@ export default function PageEditor({
     ];
   }, [remoteProvider, currentUser?.user]);
 
+  const debouncedSendSaveCommand = useDebouncedCallback(() => {
+    const payload = 'forceSave';
+    remoteProvider.sendStateless(payload);
+  }, 300);
+
   const editor = useEditor(
     {
       extensions,
@@ -224,6 +229,7 @@ export default function PageEditor({
           keydown: (_view, event) => {
             if ((event.ctrlKey || event.metaKey) && event.code === 'KeyS') {
               event.preventDefault();
+              debouncedSendSaveCommand();
               return true;
             }
             if (["ArrowUp", "ArrowDown", "Enter"].includes(event.key)) {
