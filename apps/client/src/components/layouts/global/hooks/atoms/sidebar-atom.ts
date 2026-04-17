3d9ff434fb8e644667c49ce2f43b6da4d9a9a592
Nikolay Bely <belyynikolay@gmail.com>
2026-04-17 09:15:18 +0700

    2075 - feat: Comment section variable width

--- apps/client/src/components/layouts/global/hooks/atoms/sidebar-atom.ts
+++ apps/client/src/components/layouts/global/hooks/atoms/sidebar-atom.ts
@@ -20,4 +20,6 @@ export const asideStateAtom = atomWithWebStorage<AsideStateType>("aSide",{
   isAsideOpen: false,
 });
 
-export const sidebarWidthAtom = atomWithWebStorage<number>('sidebarWidth', 300);
+export const sidebarWidthAtom = atomWithWebStorage<number>("sidebarWidth", 300);
+
+export const asideWidthAtom = atomWithWebStorage<number>("asideWidth", 350);
